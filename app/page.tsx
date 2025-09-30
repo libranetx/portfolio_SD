51e7d11aaa46668b46442eac9098620d45800d31
libranetx <tadiostsegaye7@gmail.com>
2025-09-30 17:42:21 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,93 @@
+
+"use cliant"
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Download, Github, Linkedin, Mail, Twitter } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import image1 from "../../public/imge1.png"
+import image2 from "../../public/image2.png"
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <div className="min-h-screen flex gradient-bg"> 
+          <section className="section-padding min-h-screen flex items-center" >
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
+              <div className="space-y-8 ml-10" > 
+                <div className="space-y-8 ">
+                  
+                  <p className="font-mono text-lg uppercase tracking-wider text-gray-500">Hello i'm</p>
+                  <h1 className="text-7xl lg:text-8xl font-bold text-gray-200 leading-tight">
+                    Tadios
+                    <br />
+                    <span className="gradient-text">TSEGAYE</span>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                  </h1>
+
+                  <p className="text-xl text-muted-foreground max-w-lg ">Professional Softwere and web application devloper </p>
+                 
+                </div>
+                <p className="text-lg text-muted-foreground max-w-lg leading-relaxed">I am a dedicated Software Developer and Web Application Developer 
+                  with a strong focus on creating scalable, 
+                  efficient, and user-centered digital solutions. 
+                  My work spans both front-end and back-end development, 
+                  enabling me to deliver complete applications 
+                  that are functional, secure, and visually engaging.</p>
+
+                <div className="flex flex-col sm:flex-row gap-5">
+                  <Link href="/projects">
+                  <Button variant="outline">
+                      My Work
+                      <ArrowRight className="ml-2 h-4 w-4 group-hover:translate-x-1 transition-transform"/>
+                  </Button>
+                  </Link>
+                  
+
+                  <Button variant="outline">
+                      Download CV
+                      <Download className="ml-2 h-4 w-4 "/>
+                  </Button>
+
+                </div>
+
+
+                <div className="flex item-center space-x-9 pt-8 ml-10">
+                  <a href="#" className="text-muted-foreground hover:text-primary transition-colors">
+                    <Github size={24}/>
+                  </a>
+                  <a href="#" className="text-muted-foreground hover:text-primary transition-colors">
+                    <Linkedin size={24}/>
+                  </a>
+                  <a href="#" className="text-muted-foreground hover:text-primary transition-colors">
+                    <Mail size={24}/>
+                  </a>
+                  <a href="#" className="text-muted-foreground hover:text-primary transition-colors">
+                    <Twitter size={24}/>
+                  </a>
+
+
+                </div>
+
+              </div>
+
+              <div className="relative">
+                <div className="relative z-10">
+                  <Image 
+                    src="/imge1.png"
+                    alt="Profile Picture" 
+                    width={600} 
+                    height={600} 
+                    className="rounded-2xl object-cover"
+                    priority
+                  />
+                </div>
+                
+            
+                
+              </div>
+
+            </div>
+          </section>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
   );
 }
